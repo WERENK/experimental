7b0442fbc96f70534a1bb6bca17f979cc511864f
MAS GUS <gusti_ex@yahoo.co.id>
2014-06-30 18:44:22 +0700

    june 30, 2

---
+++ krt.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+	
+		<title><?php echo $title;?></title>
+		<link rel="stylesheet" href="<?php echo base_url(); ?>ext421/resources/css/ext-all-gray.css" type="text/css" />
+        <script type="text/javascript" src="<?php echo base_url(); ?>extj421/ext-debug.js"></script>
+        <script type="text/javascript" src="<?php echo base_url(); ?>app/antanusa/antanusa.js" ></script>
+	
+	</head>
+    <body></body>
+</html>
